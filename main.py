5f36cda11589c031f1c613513a1956caeab4142d
yeongsunpark <ylunar@naver.com>
2023-04-05 21:59:49 +0900

    의약품사용정보조회서비스 추가

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 @st.cache_data
 def load_data(file_name):
-    data = pd.read_csv(file_name)
+    data = pd.read_csv(file_name, encoding='cp949')
     return data
 
 
@@ -22,75 +22,75 @@ st.set_page_config(layout="wide", initial_sidebar_state="auto", page_title="Hosp
 st.title('병원 정보 검색 서비스🏥')
 
 all_df = load_data("hospital_info.csv")
-all_df.sort_values(by=['개업일자'], axis=0)
+all_df.sort_values(by=['estbDd'], axis=0)
 t_input = st.text_input(label="병원명 검색", key="a")  # session state key = 'a'
-search_df = all_df.query('병원이름.str.contains("{}")'.format(t_input))  ## df.query(조건식 문자열)
+search_df = all_df.query('yadmNm.str.contains("{}")'.format(t_input))  ## df.query(조건식 문자열)
 
 # 여러개 선택할 수 있을 때는 multiselect를 이용하실 수 있습니다. (return : list)
-clcd_list = all_df['급2'].unique().tolist()
-select_multi_clcd = st.sidebar.multiselect('확인하고자 하는 clcd를 선택해 주세요. 복수선택가능', clcd_list, default=['의원'])
+clcd_list = all_df['clCdNm'].unique().tolist()
+select_multi_clcd = st.sidebar.multiselect('확인하고자 하는 clcd를 선택해 주세요. 복수선택가능', clcd_list)
 
 # 사이드바에 select box를 활용하여 조건을 선택한 다음 그에 해당하는 행만 추출하여 데이터프레임을 만들고자합니다.
 st.sidebar.title('지역 선택📍')
-sido_list = all_df['시도'].unique().tolist()
+sido_list = all_df['sidoCdNm'].unique().tolist()
 select_multi_sido = st.sidebar.multiselect('확인하고자 하는 시도를 선택해 주세요. 복수선택가능', sido_list)
-sido_df = all_df[all_df['시도'].isin(select_multi_sido)]  # 선택된 시도
+sido_df = all_df[all_df['sidoCdNm'].isin(select_multi_sido)]  # 선택된 sidoCdNm
 
 # all_df, search_df, sido_df
 if select_multi_clcd:
-    all_df = all_df[all_df['급2'].isin(select_multi_clcd)]
-    search_df = search_df[search_df['급2'].isin(select_multi_clcd)]
-    sido_df = sido_df[sido_df['급2'].isin(select_multi_clcd)]
+    all_df = all_df[all_df['clCdNm'].isin(select_multi_clcd)]
+    search_df = search_df[search_df['clCdNm'].isin(select_multi_clcd)]
+    sido_df = sido_df[sido_df['clCdNm'].isin(select_multi_clcd)]
 
-if select_multi_sido:  # 시도 선택된 "상태"
-    sggu_list = sido_df['시군구'].unique().tolist()  # 선택된 시도의 시군구 리스트
+if select_multi_sido:  # sidoCdNm 선택된 "상태"
+    sggu_list = sido_df['sgguCdNm'].unique().tolist()  # 선택된 sidoCdNm의 sgguCdNm 리스트
     select_multi_sggu = st.sidebar.multiselect('확인하고자 하는 시군구를 선택해 주세요. 복수선택가능', sggu_list)
-    sggu_df = sido_df[sido_df['시군구'].isin(select_multi_sggu)]  # 선택된 시군구 df
+    sggu_df = sido_df[sido_df['sgguCdNm'].isin(select_multi_sggu)]  # 선택된 sgguCdNm df
     if select_multi_clcd:
-        sggu_df = sggu_df[sggu_df['급2'].isin(select_multi_clcd)]
+        sggu_df = sggu_df[sggu_df['clCdNm'].isin(select_multi_clcd)]
 
-    if select_multi_sggu:  # 시군구 선택된 "상태"
-        emdong_list = sggu_df['읍면동명'].unique().tolist()  # 선택된 시군구의 읍면동 리스트
+    if select_multi_sggu:  # sgguCdNm 선택된 "상태"
+        emdong_list = sggu_df['emdongNm'].unique().tolist()  # 선택된 sgguCdNm의 읍면동 리스트
         select_multi_emdong = st.sidebar.multiselect('확인하고자 하는 읍면동을 선택해 주세요. 복수선택가능', emdong_list)
-        emdong_df = sggu_df[sggu_df['읍면동명'].isin(select_multi_emdong)]  # 선택된 읍면동 df
+        emdong_df = sggu_df[sggu_df['emdongNm'].isin(select_multi_emdong)]  # 선택된 읍면동 df
         if select_multi_clcd:
-            emdong_df = emdong_df[emdong_df['급2'].isin(select_multi_clcd)]
+            emdong_df = emdong_df[emdong_df['clCdNm'].isin(select_multi_clcd)]
 
         if select_multi_emdong:  # 읍면동 선택된 "상태"
             if st.session_state["a"] == "":
                 st.write('사이드바 조건으로 선택된 데이터: 총 {}건 (최대 10건만 출력됨)'.format(len(emdong_df)))
-                st.table(emdong_df.head(10))  # 선택된 읍면동 df 출력
+                st.dataframe(emdong_df.head(10))  # 선택된 읍면동 df 출력
                 file_download(df=emdong_df, file_tag=None)
             else:
-                emdong_df2 = emdong_df.query('병원이름.str.contains("{}")'.format(t_input))
+                emdong_df2 = emdong_df.query('yadmNm.str.contains("{}")'.format(t_input))
                 st.write('사이드바 조건 중 "{}"(으)로 검색된 데이터: 총 {}건 (최대 10건만 출력됨)'.format(t_input, len(emdong_df2)))
-                st.table(emdong_df2.head(10))  # 선택된 읍면동+병원명조건 df 출력
+                st.dataframe(emdong_df2.head(10))  # 선택된 읍면동+병원명조건 df 출력
                 file_download(df=emdong_df2, file_tag=t_input)
-        else:  # 읍면동 선택 안 된 "상태" -> 선택된 시군구 df 출력
+        else:  # 읍면동 선택 안 된 "상태" -> 선택된 sgguCdNm df 출력
             if st.session_state["a"] == "":
                 st.write('사이드바 조건으로 선택된 데이터: 총 {}건 (최대 10건만 출력됨)'.format(len(sggu_df)))
-                st.table(sggu_df.head(10))  # 선택된 시군구 df 출력
+                st.dataframe(sggu_df.head(10))  # 선택된 sgguCdNm df 출력
                 file_download(df=sggu_df, file_tag=None)
             else:
-                sggu_df2 = sggu_df.query('병원이름.str.contains("{}")'.format(t_input))
+                sggu_df2 = sggu_df.query('yadmNm.str.contains("{}")'.format(t_input))
                 st.write('사이드바 조건 중 "{}"(으)로 검색된 데이터: 총 {}건 (최대 10건만 출력됨)'.format(t_input, len(sggu_df2)))
-                st.table(sggu_df2.head(10))  # 선택된 시군구+병원명조건 df 출력
+                st.dataframe(sggu_df2.head(10))  # 선택된 sgguCdNm+병원명조건 df 출력
                 file_download(df=sggu_df2, file_tag=t_input)
-    else:  # 시군구 선택 안 된 "상태" -> 선택된 시도 df 출력
+    else:  # sgguCdNm 선택 안 된 "상태" -> 선택된 sidoCdNm df 출력
         if st.session_state["a"] == "":
             st.write('사이드바 조건으로 선택된 데이터: 총 {}건 (최대 10건만 출력됨)'.format(len(sido_df)))
-            st.table(sido_df.head(10))  # 선택된 시도 df 출력
+            st.dataframe(sido_df.head(10))  # 선택된 sidoCdNm df 출력
             file_download(df=sido_df, file_tag=None)
         else:
-            sido_df2 = sido_df.query('병원이름.str.contains("{}")'.format(t_input))
+            sido_df2 = sido_df.query('yadmNm.str.contains("{}")'.format(t_input))
             st.write('사이드바 조건 중 "{}"(으)로 검색된 데이터: 총 {}건 (최대 10건만 출력됨)'.format(t_input, len(sido_df2)))
-            st.table(sido_df2.head(10))  # 선택된 시도+병원명조건 df 출력
+            st.dataframe(sido_df2.head(10))  # 선택된 sidoCdNm+병원명조건 df 출력
             file_download(df=sido_df2, file_tag=t_input)
-else:  # 시도 선택 안 된 "상태" -> 전체 df 출력
+else:  # sidoCdNm 선택 안 된 "상태" -> 전체 df 출력
     if st.session_state["a"] == "":
         st.write('전국 데이터: 총 {}건 (최대 10건만 출력됨)'.format(len(all_df)))
-        st.table(all_df.head(10))
+        st.dataframe(all_df.head(10))
     else:
         st.write('전국 데이터 중 "{}"(으)로 검색된 데이터: 총 {}건 (최대 10건만 출력)'.format(t_input, len(search_df)))
-        st.table(search_df.head(10))
+        st.dataframe(search_df.head(10))
         file_download(df=search_df, file_tag=t_input)
---
+++ pages/1_drug_use.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.figure_factory as ff
+import numpy as np
+import time
+
+
+
+@st.cache_data
+def load_data(file_name):
+    data = pd.read_csv(file_name, encoding='cp949')
+    return data
+
+
+st.set_page_config(layout="wide", initial_sidebar_state="auto", page_title="msup User Info Service", page_icon="💊")
+st.title('의약품사용정보조회서비스💊')
+
+all_df = load_data("drug_use.csv")
+all_df['diagYm'] = pd.to_datetime(all_df['diagYm'], format='%Y%m')
+
+gnl_list = all_df['gnlNmCd'].unique().tolist()
+select_multi_gnl = st.sidebar.multiselect('확인하고자 하는 gnl_nm_cd를 선택해 주세요. 복수선택가능', gnl_list)
+
+sido_list = all_df['sidoCdNm'].unique().tolist()
+select_multi_sido = st.sidebar.multiselect('확인하고자 하는 sido_cd_nm을 선택해 주세요. 복수선택가능', sido_list)
+
+sggu_list = all_df['sgguCdNm'].unique().tolist()
+select_multi_sggu = st.sidebar.multiselect('확인하고자 하는 sggu_cd_nm을 선택해 주세요. 복수선택가능', sggu_list)
+
+genre = st.sidebar.radio(
+    "확인하고자 하는 값을 선택해주세요.",
+    ('msupUseAmt', 'totUseQty'))
+
+df = all_df[all_df['gnlNmCd'].isin(select_multi_gnl)]
+df = df[df['sidoCdNm'].isin(select_multi_sido)]
+df = df[df['sgguCdNm'].isin(select_multi_sggu)]
+
+# Create distplot with custom bin_size
+fig = px.line(df, x='diagYm', y=genre, color='insupTpCd', markers=True)
+fig.update_layout(
+    title = '성분별의료기관종별사용량목록조회',
+    xaxis_tickformat = '%B<br>%Y')
+fig.update_layout(
+    xaxis = dict(
+        tickmode = 'array',
+        tickvals = df['diagYm'],
+        ticktext = df['diagYm'].dt.strftime('%Y-%m')
+    )
+)
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
